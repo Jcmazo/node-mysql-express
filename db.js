a2b133cd0a6abc29571457caf884770b096f5d1a
Gamani-Sai <josephpoets@gmail.com>
2023-05-17 14:55:45 -0500

    feature:database

---
+++ db.js
@@ -0,0 +1,11 @@
+// createpool se usa para un conjunto de conxiones
+import {createPool} from "mysql2";
+
+//Este codigo es equivalente a createconexion
+export const pool = createPool({
+  host: 'localhost',
+  user: 'root',
+  password: 'jcmazo',
+  port: 3306,
+  database: 'companydb'
+})
